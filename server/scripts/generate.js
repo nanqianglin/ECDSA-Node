2047c998499a442bb49bf5975f27a8e4554fe9db
nanqiang <nanqianglin@gmail.com>
2024-04-24 11:43:40 +0800

    feat: add the generate random private key script

---
+++ server/scripts/generate.js
@@ -0,0 +1,15 @@
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
+const privKey = secp256k1.utils.randomPrivateKey();
+const pubKey = secp256k1.getPublicKey(privKey);
+
+function getAddress(publicKey) {
+  const hash = keccak256(publicKey.slice(1));
+  return '0x' + toHex(hash.slice(-20));
+}
+
+console.log('private key:', toHex(privKey));
+console.log('public key:', toHex(pubKey));
+console.log('address: ', getAddress(pubKey));
